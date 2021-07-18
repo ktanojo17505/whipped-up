7dc2299539a6e6c926366500d412141d8b41a37f
Kiara Tanojo <ktanojo@umich.edu>
2021-07-19 00:12:07 +0700

    added star and finished navbar

--- pages/results.js
+++ pages/results.js
@@ -8,7 +8,7 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "next/image";
-import Logo from "../public/images/Logo.svg"
+import logofinal from "../public/images/logofinal.svg";
 
 
 const apiKey = process.env.SPOONACULAR_API;
@@ -167,21 +167,21 @@ function Results() {
     
     return (
         <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light static-top" style={{border: "2px solid black", backgroundColor: "white !important", height: "100px"}}>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light static-top" style={{border: "2px solid #332F30", backgroundColor: "white !important", height: "100px", padding: "0"}}>
             {/* <div className="container" style={{marginLeft: 0}}> */}
                 <a className="navbar-brand" style={{marginLeft: "14px"}}href="#">
-                    <Image src={Logo} alt="" width="70px" height="64px" top="20px"/>
+                    <Image src={logofinal} alt="" width="70px" height="64px" top="20px"/>
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive" style={{justifyContent:"flex-end", marginRight: "14px"}}>
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                        <a className="nav-link" style={{fontSize:"26px", fontFamily:"Le Havre Rounded Regular"}} href="#">about us</a>
+                <div className="collapse navbar-collapse" id="navbarResponsive" style={{justifyContent:"flex-end", height: "100%"}}>
+                    <ul className="navbar-nav ml-auto" style={{height: "100%", display: "flex", justifyContent:"center", alignItems:"center"}}>
+                        <li className="nav-item active" style={{padding: "22px", border:"2px solid #332F30", borderBottom: "0", width: "250px"}}>
+                            <a className="nav-link" style={{fontSize:"26px", fontFamily:"Le Havre Rounded Regular", color: "#332F30", textAlign:"center"}} href="#">about us</a>
                         </li>
-                        <li className="nav-item">
-                        <a className="nav-link" style={{fontSize:"26px", fontFamily:"Le Havre Rounded Regular"}} href="#">back to home</a>
+                        <li className="nav-item" style={{padding: "22px", backgroundColor: "#332F30", width: "250px"}}>
+                            <a className="nav-link" style={{fontSize:"26px", fontFamily:"Le Havre Rounded Regular", color:"white", textAlign:"center"}} href="#">back to home</a>
                         </li>
                     </ul>
                 </div>
