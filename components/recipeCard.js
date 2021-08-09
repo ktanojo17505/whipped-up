e6a93667c3b51b343853f7052032a55cad0aa32e
Kiara Tanojo <ktanojo@umich.edu>
2021-08-09 21:06:40 +0700

    uncommented the requests todo: add loader for while data is loading / fetching

--- components/recipeCard.js
+++ components/recipeCard.js
@@ -4,18 +4,24 @@ import { Radar } from 'react-chartjs-2';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function RecipeCard(props) {
-    const data = {
-        labels: ["Sweet","Salty","Sour","Bitter","Savory","Fatty"],
-        datasets: [
-          {
-              label:"",
-              backgroundColor:"rgb(75, 192, 192, 0.2)",
-              borderColor:"rgb(75, 192, 192)",
-              pointBackgroundColor:"rgb(75, 192, 192)",
-              data: [20.31,100,43.67,26.8,30.94,53.45],
-          },
-        ],
-    };
+    const [radarData, setRadarData] = useState(null);
+
+    useEffect(() => {
+        console.log(props.widgetData)
+        const data = {
+            labels: ["Sweet","Salty","Sour","Bitter","Savory","Fatty"],
+            datasets: [
+              {
+                  label:"",
+                  backgroundColor:"rgb(75, 192, 192, 0.2)",
+                  borderColor:"rgb(75, 192, 192)",
+                  pointBackgroundColor:"rgb(75, 192, 192)",
+                  data: props.widgetData,
+              },
+            ],
+        };
+        setRadarData(data);
+    }, [props])
 
     const options = {
         legend:{display: !1},
@@ -28,24 +34,17 @@ function RecipeCard(props) {
         plugins:{
             legend: false
         },
-    }
-    const [isLongSummary, setIsLongSummary] = useState(false);
-    
+    }    
+
     return (
         <div className={`.col ${styles.recipeCardContainer}`}>
             <p className={styles.recipeTitle}>{props.title}</p>
             <div style={{padding:"0px 30px 30px 30px"}}>
                 <Radar 
-                    data={data} 
+                    data={radarData} 
                     options={options}
                 />
-                {
-                    isLongSummary && 
-                    <p className={styles.recipeSummary}>{props.summary}</p>
-                }{
-                    !isLongSummary &&
-                    <p className={styles.recipeSummary}>{props.shortSummary}</p>
-                }
+                <p className={styles.recipeSummary}>{props.summary}</p>
                 <div className={styles.recipeFooter}>
                     <p className={styles.recipeTime} stye={{margin: '0'}}>{props.time} minutes</p>
                     <button className={styles.viewRecipeButton}>
--- pages/results.js
+++ pages/results.js
@@ -21,7 +21,6 @@ function Results() {
     const router = useRouter();
     const queries = router.query;
     const [info, setInfo] = useState([]);
-    const [widgets, setWidgets] = useState([]); 
     const [numResults, setNumResults] = useState(0);
 
     useEffect(() =>{
@@ -65,98 +64,100 @@ function Results() {
         }
         // *DELETE LATER temp recipe ids array so that wont continuously fetch api 
         // var recipeIds = [643150, 649280, 607953, 73449, 659081, 651979, 157960, 634554, 665524, 660843]
-        var recipeIds = [643150, 649280]
+        // var recipeIds = [643150, 649280]
 
         /** DONT DELETE grabs recipe ID's */
-        // var recipeIds = [];
-        // recipeIds = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API}` + params)
-        // .then(response => response.json())
-        // .then(data => {
-        //     data.map((recipe) => {
-        //         recipeIds.push(recipe.id);
-        //     })
-        //     return recipeIds;
-        // });
-        // console.log(recipeIds);
+        var recipeIds = [];
+        recipeIds = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API}` + params)
+        .then(response => response.json())
+        .then(data => {
+            data.map((recipe) => {
+                recipeIds.push(recipe.id);
+            })
+            return recipeIds;
+        });
         /** DONT DELETE */
 
         /** DONT DELETE grabs info */
-        // let promisesInfo = [];
-        // recipeIds.map((id) => {
-        //     promisesInfo.push(fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API}&includeNutrition=false`));
-        // })
-        // var info = await Promise.all(promisesInfo)
-        // .then(async (responses) => {
-        //     var dataArr = [];
-        //     for (let res of responses){
-        //         dataArr.push(await res.json());
-        //     }
-        //     return dataArr; 
-        // }).then(data => {
-        //     return data;
-        // });
+        let promisesInfo = [];
+        recipeIds.map((id) => {
+            promisesInfo.push(fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API}&includeNutrition=false`));
+        })
+        var info = await Promise.all(promisesInfo)
+        .then(async (responses) => {
+            var dataArr = [];
+            for (let res of responses){
+                dataArr.push(await res.json());
+            }
+            return dataArr; 
+        }).then(data => {
+            return data;
+        });
+        var entries = [];
+        for (let entry of info){
+            let obj = {
+                "id": entry.id,
+                "title": entry.title,
+                "url": entry.sourceUrl,
+                "time": entry.readyInMinutes,
+                "summary": entry.summary.substr(0, entry.summary.indexOf('.'))
+            }; 
+            entries.push(obj);
+        }        
+
+        /* DONT DELETE FETCHES WIDGET API */ 
+        let promisesWidget = [];
+        recipeIds.map((id) => {
+            promisesWidget.push(fetch(`https://api.spoonacular.com/recipes/${id}/tasteWidget?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API}`));
+        })
+        var dataArr = await Promise.all(promisesWidget)
+        .then(async(responses) => {
+            var widgetArr = [];
+            for (let res of responses){
+                widgetArr.push(await res.text());
+            }
+            return widgetArr
+        }).then(widgetArr => {
+            let dataArr = [];
+            for (let widget of widgetArr){
+                console.log(widget);
+                let arr = []
+                let startIdx = 305; // taking advantage of the fact that all responses are the same except for the datapoints
+                let endIdx = widget.indexOf("]", startIdx)-1; // exclude the bracket
+                let dataArrStr = widget.substr(startIdx, endIdx-startIdx).split(',');
+                console.log(dataArrStr);
+                for (const datapoint of dataArrStr){
+                    arr.push(parseFloat(datapoint))
+                }
+                dataArr.push(arr);
+            }
+            return dataArr;
+        });
+
+        dataArr.map((data, index) => {
+            entries[index].widgetData = data; 
+        })
+        setInfo(entries);
+        setNumResults(entries.length);
+
+        // DUMMY DATA 
         // var entries = [];
-        // for (let entry of info){
-        //     let obj = {
-        //         "id": entry.id,
-        //         "title": entry.title,
-        //         "url": entry.sourceUrl,
-        //         "time": entry.readyInMinutes,
-        //         "summary": entry.summary
-        //     }; 
+        // let obj = {
+        //     "id": 643150,
+        //     "title": "Fluffy frittata with spinach",
+        //     "url": "http://www.foodista.com/recipe/Z3SVPNCV/fluffy-frittata-with-spinach",
+        //     "time": 45,
+        //     "summary": <>Fluffy frittata with spinach might be just the main course you are searching for. This gluten free, primal, and ketogenic recipe serves 4 and costs <b>$1.55 per serving</b>. One serving contains <b>279 calories</b>, <b>20g of protein</b>, and <b>20g of fat</b>. A mixture of spinach, olive oil, spinach, and a handful of other ingredients are all it takes to make this recipe so tasty. 
+        //                     To use up the eggs you could follow this main course with the <a href="https://spoonacular.com/recipes/rose-levy-beranbaums-chocolate-tomato-cake-with-mystery-ganache-27408">Rose Levy Beranbaum's Chocolate Tomato Cake with Mystery Ganache</a> as a dessert. Only a few people made this recipe, and 7 would say it hit the spot. From preparation to the plate, this recipe takes roughly 
+        //                     <b>45 minutes</b>. All things considered, we decided this recipe <b>deserves a spoonacular score of 74%</b>. This score is pretty good. Try <a href="https://spoonacular.com/recipes/fluffy-bacon-cheese-frittata-147451">Fluffy Bacon-Cheese Frittata</a>, <a href="https://spoonacular.com/recipes/fluffy-gluten-free-spinach-cheese-biscuits-682018">Fluffy Gluten Free Spinach Cheese Biscuits</a>, 
+        //                     and <a href="https://spoonacular.com/recipes/fluffy-light-yummy-spinach-blue-cheese-souffle-427654">Fluffy, Light & Yummy: Spinach & Blue Cheese Souffle</a> for similar recipes</>,
+        //     "shortSummary": <>Fluffy frittata with spinach might be just the main course you are searching for.</>
+        // }
+        // for (let i = 0; i < 6; ++i){
         //     entries.push(obj);
         // }
         // setInfo(entries);
-        
-        /** DONT DELETE */
-
-        /* DONT DELETE FETCHES WIDGET API */ 
-        // let promisesWidget = [];
-        // recipeIds.map((id) => {
-        //     promisesWidget.push(fetch(`https://api.spoonacular.com/recipes/${id}/tasteWidget?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API}`));
-        // })
-        // var dataArr = await Promise.all(promisesWidget)
-        // .then(async(responses) => {
-        //     var widgetArr = [];
-        //     for (let res of responses){
-        //         widgetArr.push(await res.text());
-        //     }
-        //     return widgetArr
-        // }).then(widgetArr => {
-        //     let dataArr = [];
-        //     for (let widget of widgetArr){
-        //         let arr = []
-        //         let startIdx = 305; // taking advantage of the fact that all responses are the same except for the datapoints
-        //         let endIdx = widget.indexOf("]", startIdx)-1; // exclude the bracket
-        //         let dataArrStr = widget.substr(startIdx, endIdx-startIdx).split(',');
-        //         for (const datapoint of dataArrStr){
-        //             arr.push(parseFloat(datapoint))
-        //         }
-        //         dataArr.push(arr);
-        //     }
-        //     return dataArr;
-        // });
-        // console.log(dataArr[0]);
-        // console.log(dataArr[1]);
-
-        // DUMMY DATA 
-        var entries = [];
-        let obj = {
-            "id": 643150,
-            "title": "Fluffy frittata with spinach",
-            "url": "http://www.foodista.com/recipe/Z3SVPNCV/fluffy-frittata-with-spinach",
-            "time": 45,
-            "summary": <>Fluffy frittata with spinach might be just the main course you are searching for. This gluten free, primal, and ketogenic recipe serves 4 and costs <b>$1.55 per serving</b>. One serving contains <b>279 calories</b>, <b>20g of protein</b>, and <b>20g of fat</b>. A mixture of spinach, olive oil, spinach, and a handful of other ingredients are all it takes to make this recipe so tasty. 
-                            To use up the eggs you could follow this main course with the <a href="https://spoonacular.com/recipes/rose-levy-beranbaums-chocolate-tomato-cake-with-mystery-ganache-27408">Rose Levy Beranbaum's Chocolate Tomato Cake with Mystery Ganache</a> as a dessert. Only a few people made this recipe, and 7 would say it hit the spot. From preparation to the plate, this recipe takes roughly 
-                            <b>45 minutes</b>. All things considered, we decided this recipe <b>deserves a spoonacular score of 74%</b>. This score is pretty good. Try <a href="https://spoonacular.com/recipes/fluffy-bacon-cheese-frittata-147451">Fluffy Bacon-Cheese Frittata</a>, <a href="https://spoonacular.com/recipes/fluffy-gluten-free-spinach-cheese-biscuits-682018">Fluffy Gluten Free Spinach Cheese Biscuits</a>, 
-                            and <a href="https://spoonacular.com/recipes/fluffy-light-yummy-spinach-blue-cheese-souffle-427654">Fluffy, Light & Yummy: Spinach & Blue Cheese Souffle</a> for similar recipes</>,
-            "shortSummary": <>Fluffy frittata with spinach might be just the main course you are searching for.</>
-        }
-        for (let i = 0; i < 6; ++i){
-            entries.push(obj);
-        }
-        setInfo(entries);
-        setNumResults(entries.length);
+        // setNumResults(entries.length);
     }
 
     
@@ -175,7 +176,7 @@ function Results() {
                             url={entry.url}
                             time={entry.time}
                             summary={entry.summary}
-                            shortSummary={entry.shortSummary}
+                            widgetData={entry.widgetData}
                         />
                     )}
                 </div>
